3cc25b0b2c07ad4220c1c96c53f01dc9d8a46996
mareiski <marco.reisinger@t-online.de>
2021-08-23 18:28:56 +0200

    hopefully fixed function

---
+++ netlify/functions/getRiverRoute.ts
@@ -0,0 +1,22 @@
+const riverRoute = require("../../src/assets/riverRoute.ts");
+
+exports.handler = async function(event) {
+  const {
+    startLocation,
+    endLocation,
+    rawRivers,
+    rawEuropeanRivers
+  } = JSON.parse(event.body);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      riverRoute.getRiverRoute(
+        startLocation,
+        endLocation,
+        [rawRivers, rawEuropeanRivers],
+        []
+      )
+    )
+  };
+};

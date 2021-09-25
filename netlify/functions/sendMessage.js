f647264630fb4af6675f1cf06794cbdb21b0a9e6
mareiski <marco.reisinger@t-online.de>
2021-09-25 15:59:26 +0200

    fixed function

--- netlify/functions/sendMessage.js
+++ netlify/functions/sendMessage.js
@@ -4,13 +4,27 @@ exports.handler = async function(event) {
   const token = event.queryStringParameters.token;
   const message = JSON.parse(event.queryStringParameters.message);
 
+  const cred = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(cred),
     databaseURL: "https://roundtrips4you.firebaseio.com",
-    name: "admin"
+    name: "adminMessaging"
   });
 
   const messaging = admin.messaging();
 
-  messaging.sendToDevice(token, message);
+  await messaging.sendToDevice(token, message);
+
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET"
+  };
+
+  return {
+    statusCode: 200,
+    headers,
+    body: "message sent"
+  };
 };
